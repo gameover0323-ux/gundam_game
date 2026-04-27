c03350becfd3632dcdd0c647e47aa572cbfb4485
gameover0323-ux <gameover0323@gmail.com>
2026-04-27 15:21:52 +0900

    Create js_units_index.js to export unitList

---
+++ js_units_index.js
@@ -0,0 +1,13 @@
+import { gundam_mc } from "./js_units_gundam_mc.js";
+import { z_gundam } from "./js_units_z_gundam.js";
+import { shining_gundam } from "./js_units_shining_gundam.js";
+import { wing_zero } from "./js_units_wing_zero.js";
+
+
+
+export const unitList = [
+  gundam_mc,
+  z_gundam,
+  shining_gundam,
+  wing_zero
+];
