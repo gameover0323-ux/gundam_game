eb3b65e058b98b9ad8276a1b5717f43309741b87
gameover0323-ux <gameover0323@gmail.com>
2026-04-28 22:57:23 +0900

    Update js_units_index.js

--- js/js_units_index.js
+++ js/js_units_index.js
@@ -4,6 +4,8 @@ import { shining_gundam } from "./js_units_shining_gundam.js";
 import { wing_zero } from "./js_units_wing_zero.js";
 
 
+//ボス機体//
+import { devil_gundam } from "./js_units_devil_gundam.js";
 
 export const unitList = [
   gundam_mc,
@@ -11,3 +13,7 @@ export const unitList = [
   shining_gundam,
   wing_zero
 ];
+
+export const bossList = [ 
+  devil_gundam
+  ];
