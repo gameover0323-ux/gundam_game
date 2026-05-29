58b90e037fd349e23e2316ad0d09d183091d4d76
gameover0323-ux <gameover0323@gmail.com>
2026-05-29 13:12:43 +0900

    Create js_units_cpu_v2_gundam.js

---
+++ js/js_units_cpu_v2_gundam.js
@@ -0,0 +1,5 @@
+import { v2_gundam } from "./js_units_v2_gundam.js";
+
+export const cpu_v2_gundam = JSON.parse(JSON.stringify(v2_gundam));
+cpu_v2_gundam.id = "cpu_v2_gundam";
+cpu_v2_gundam.name = "CPU V2ガンダム";
