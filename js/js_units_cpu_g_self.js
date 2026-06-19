5aab23d440320a4bbcc39e256c12212ba941f7ac
gameover0323-ux <gameover0323@gmail.com>
2026-06-19 12:23:59 +0900

    Rename cpu_g_self name to 'G-セルフ'

--- js/js_units_cpu_g_self.js
+++ js/js_units_cpu_g_self.js
@@ -2,4 +2,4 @@ import { g_self } from "./js_units_g_self.js";
 
 export const cpu_g_self = structuredClone(g_self);
 cpu_g_self.id = "cpu_g_self";
-cpu_g_self.name = "CPU Gセルフ 宇宙用パック";
+cpu_g_self.name = "G-セルフ";
