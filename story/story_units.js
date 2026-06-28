64f88d4f85d6d7061a3bb9b5e34b9ac60b5cc921
gameover0323-ux <gameover0323@gmail.com>
2026-06-28 15:50:17 +0900

    Update story_units.js

--- story/story_units.js
+++ story/story_units.js
@@ -1,21 +1,59 @@
 import { buildProtoCreateGundamUnit } from "../js/js_units_proto_create_gundam.js";
 import { loadStorySave } from "./story_save.js";
 
-export function getStoryCreateUnit(unitId) {
-  const save = loadStorySave();
+const storyCreateUnitLabOverrides = {};
 
+function clone(value) {
+  return JSON.parse(JSON.stringify(value));
+}
+
+function buildSaveWithLabOverride(unitId) {
+  const save = loadStorySave();
+  const overrideLab = storyCreateUnitLabOverrides[unitId];
+
+  if (!overrideLab) {
+    return save;
+  }
+
+  return {
+    ...save,
+    createUnits: {
+      ...(save.createUnits || {}),
+      [unitId]: {
+        ...(save.createUnits?.[unitId] || {}),
+        lab: clone(overrideLab)
+      }
+    }
+  };
+}
+
+export function setStoryCreateUnitLabOverride(unitId, labState) {
+  if (!unitId || !labState) return;
+  storyCreateUnitLabOverrides[unitId] = clone(labState);
+}
+
+export function clearStoryCreateUnitLabOverride(unitId) {
+  if (!unitId) return;
+  delete storyCreateUnitLabOverrides[unitId];
+}
+
+export function clearAllStoryCreateUnitLabOverrides() {
+  Object.keys(storyCreateUnitLabOverrides).forEach(unitId => {
+    delete storyCreateUnitLabOverrides[unitId];
+  });
+}
+
+export function getStoryCreateUnit(unitId) {
   if (unitId === "proto_create_gundam") {
-    return buildProtoCreateGundamUnit(save);
+    return buildProtoCreateGundamUnit(buildSaveWithLabOverride(unitId));
   }
 
   return null;
 }
 
 export function getStoryCreateUnitList() {
-  const save = loadStorySave();
-
   return [
-    buildProtoCreateGundamUnit(save)
+    buildProtoCreateGundamUnit(buildSaveWithLabOverride("proto_create_gundam"))
   ];
 }
 
