c31149b851ec33c8d630d1f616a7f326ed9040af
gameover0323-ux <gameover0323@gmail.com>
2026-06-30 05:55:04 +0900

    Update js_units_training_machine.js

--- js/js_units_training_machine.js
+++ js/js_units_training_machine.js
@@ -18,29 +18,29 @@ export const training_machine = {
 
       slots: {
         slot1: {
-          label: "演習攻撃 5ダメージ",
+          label: "演習攻撃",
           desc: "5ダメージ。格闘属性。",
           effect: { type: "attack", damage: 5, count: 1, attackType: "melee" }
         },
         slot2: {
-          label: "演習射撃 5ダメージ",
+          label: "演習射撃",
           desc: "5ダメージ。射撃属性。",
           effect: { type: "attack", damage: 5, count: 1, attackType: "shoot" }
         },
         slot3: {
-          label: "演習属性攻撃 5ダメージ",
+          label: "演習属性攻撃",
           desc: "5ダメージ。射撃属性。軽減不可。",
           effect: { type: "attack", damage: 5, count: 1, attackType: "shoot", ignoreReduction: true }
         },
         slot4: {
-          label: "演習属性攻撃 5ダメージ",
+          label: "演習属性攻撃",
           desc: "5ダメージ。格闘属性。必中。",
           effect: { type: "attack", damage: 5, count: 1, attackType: "melee", cannotEvade: true }
         },
         slot5: {
-          label: "確定会心攻撃 5ダメージ",
-          desc: "5ダメージ。格闘属性。必ず会心が出る。",
-          effect: { type: "attack", damage: 5, count: 1, attackType: "melee", criticalHit: true }
+          label: "演習連続攻撃",
+          desc: "5ダメージ2回。格闘属性。",
+          effect: { type: "attack", damage: 5, count: 2, attackType: "melee", criticalHit: true }
         },
         slot6: {
           label: "回避 +1",
