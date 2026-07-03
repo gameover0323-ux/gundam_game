b4ab70d065a14b1ca765261d6411765069fbe502
gameover0323-ux <gameover0323@gmail.com>
2026-07-03 20:52:42 +0900

    Update story_units.js

--- story/story_units.js
+++ story/story_units.js
@@ -1,8 +1,40 @@
 import { buildProtoCreateGundamUnit } from "../js/js_units_proto_create_gundam.js";
+
 import { gundam_mc } from "../js/js_units_gundam_mc.js";
+import { z_gundam } from "../js/js_units_z_gundam.js";
+import { shining_gundam } from "../js/js_units_shining_gundam.js";
+import { wing_zero } from "../js/js_units_wing_zero.js";
+import { v2_gundam } from "../js/js_units_v2_gundam.js";
+import { strike_gundam } from "../js/js_units_strike_gundam.js";
+import { freedom_gundam } from "../js/js_units_freedom_gundam.js";
+import { exia } from "../js/js_units_exia.js";
+import { unicorn_gundam } from "../js/js_units_unicorn_gundam.js";
+import { g_self } from "../js/js_units_g_self.js";
+import { barbatos } from "../js/js_units_barbatos.js";
+import { aerial } from "../js/js_units_aerial.js";
+import { jegan_d_type } from "../js/js_units_jegan_d_type.js";
+import { zudah } from "../js/js_units_zudah.js";
+
 import { loadStorySave } from "./story_save.js";
 
 const storyCreateUnitLabOverrides = {};
+
+const GA_CPU_TO_PLAYABLE_UNIT_MAP = {
+  cpu_gundam_mc: gundam_mc,
+  cpu_z_gundam: z_gundam,
+  cpu_shining_gundam: shining_gundam,
+  cpu_wing_zero: wing_zero,
+  cpu_v2_gundam: v2_gundam,
+  cpu_strike_gundam: strike_gundam,
+  cpu_freedom_gundam: freedom_gundam,
+  cpu_exia: exia,
+  cpu_unicorn_gundam: unicorn_gundam,
+  cpu_g_self: g_self,
+  cpu_barbatos: barbatos,
+  cpu_aerial: aerial,
+  cpu_jegan_d_type: jegan_d_type,
+  cpu_zudah: zudah
+};
 
 function clone(value) {
   return JSON.parse(JSON.stringify(value));
@@ -51,16 +83,22 @@ function buildLiberalBaseUnit(save) {
 
 function buildLiberalGaUnit(save) {
   const activeGaUnitId = save.liberal?.activeGaUnitId || "none";
+  const playableUnit = GA_CPU_TO_PLAYABLE_UNIT_MAP[activeGaUnitId];
 
-  if (activeGaUnitId === "cpu_gundam_mc") {
-    const unit = renameUnitEverywhere(gundam_mc, save.liberal?.customName || "クリエイトガンダムリベラル");
-    unit.id = "gundam_mc";
-    unit.storyLiberalGaSourceUnitId = "cpu_gundam_mc";
-    unit.storyLiberal = true;
-    return unit;
+  if (!playableUnit) {
+    return buildLiberalBaseUnit(save);
   }
 
-  return buildLiberalBaseUnit(save);
+  const unit = renameUnitEverywhere(
+    playableUnit,
+    save.liberal?.customName || "クリエイトガンダムリベラル"
+  );
+
+  unit.id = playableUnit.id;
+  unit.storyLiberal = true;
+  unit.storyLiberalGaSourceUnitId = activeGaUnitId;
+
+  return unit;
 }
 
 export function setStoryCreateUnitLabOverride(unitId, labState) {
