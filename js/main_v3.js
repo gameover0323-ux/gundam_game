c7076d27a0e049b9b98235dad38f7a8b8fa40baf
gameover0323-ux <gameover0323@gmail.com>
2026-07-06 20:30:51 +0900

    Update main_v3.js

--- js/main_v3.js
+++ js/main_v3.js
@@ -1279,14 +1279,27 @@ function startStoryFreeBattle(modeOrConfig = "1v1", maybeOptions = {}) {
   battleInitController.init1v1(allyUnits[0], enemyUnits[0]);
 }
 
+    function forceShowStoryBattleScreen() {
     showScreen("battle");
 
-  document.getElementById("title")?.style.setProperty("display", "none");
-  document.getElementById("select")?.style.setProperty("display", "none");
-  document.getElementById("onlineRoom")?.style.setProperty("display", "none");
-  document.getElementById("battle")?.style.setProperty("display", "block");
+    document.getElementById("storyModeRoot")?.remove();
+    document.getElementById("title")?.style.setProperty("display", "none");
+    document.getElementById("select")?.style.setProperty("display", "none");
+    document.getElementById("onlineRoom")?.style.setProperty("display", "none");
+    document.getElementById("battle")?.style.setProperty("display", "block");
 
-  redrawBattleBoards();
+    redrawBattleBoards();
+  }
+
+  forceShowStoryBattleScreen();
+
+  requestAnimationFrame(() => {
+    forceShowStoryBattleScreen();
+  });
+
+  setTimeout(() => {
+    forceShowStoryBattleScreen();
+  }, 0);
 
   const buttonWrap = document.createElement("div");
   buttonWrap.id = "storyFreeBattleButtons";
