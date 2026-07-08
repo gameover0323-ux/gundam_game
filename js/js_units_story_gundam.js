e85d32aba6e3be6e5aab7571da7b8a677dbb8132
gameover0323-ux <gameover0323@gmail.com>
2026-07-08 09:55:25 +0900

    Update js_units_story_gundam.js

--- js/js_units_story_gundam.js
+++ js/js_units_story_gundam.js
@@ -2,12 +2,105 @@ export const story_gundam = {
   id: "story_gundam",
   name: "ガンダム",
   defaultFormId: "base",
-
   exp: 10,
 
   storyCompanion: {
-    unlockCondition: "ドロップ品を全て入手した状態でプロトクリエイトガンダムで撃破",
+    unlockCondition: "ドロップ品を全て入手した状態でプロトクリエイトガンダムで撃破。",
     cost: 80
+  },
+
+  storyDrops: {
+    initial: [
+      {
+        id: "story_gundam_slot2_beam_saber_energy",
+        slotKey: "slot2",
+        label: "ビームサーベル 80ダメージ",
+        cost: 15,
+        detail: "80ダメージ / 格闘 / ビーム / エネルギー / EN消費5 / 増加値15",
+        data: {
+          kind: "attack",
+          damage: 80,
+          count: 1,
+          attackType: "melee",
+          beam: true,
+          energy: true,
+          energyCost: 5,
+          energyIncrease: 15
+        }
+      }
+    ],
+
+    random: [
+      {
+        id: "story_gundam_slot5_beam_saber_energy",
+        slotKey: "slot5",
+        label: "ビームサーベル 80ダメージ",
+        cost: 15,
+        detail: "80ダメージ / 格闘 / ビーム / エネルギー / EN消費5 / 増加値15",
+        data: {
+          kind: "attack",
+          damage: 80,
+          count: 1,
+          attackType: "melee",
+          beam: true,
+          energy: true,
+          energyCost: 5,
+          energyIncrease: 15
+        }
+      },
+      {
+        id: "story_gundam_slot6_hyper_bazooka_reload",
+        slotKey: "slot6",
+        label: "ハイパーバズーカ 100ダメージ",
+        cost: 25,
+        detail: "100ダメージ / 射撃 / 軽減不可 / リロード / 弾数2 / 2ターン1リロード",
+        data: {
+          kind: "attack",
+          damage: 100,
+          count: 1,
+          attackType: "shoot",
+          ignoreReduction: true,
+          reload: true,
+          ammoMax: 2,
+          ammoCostPerUse: 1,
+          reloadTurnInterval: 2,
+          reloadPerInterval: 1
+        }
+      }
+    ],
+
+    equipment: [
+      {
+        id: "story_gundam_beam_rifle_equipment",
+        label: "ビームライフル(ガンダム)",
+        cost: 0,
+        detail: "ゲーム中3回だけ、スロット行動の代わりに80ダメージ / 射撃 / ビームの攻撃を放てる。",
+        data: {
+          kind: "equipment_attack",
+          damage: 80,
+          count: 1,
+          attackType: "shoot",
+          beam: true,
+          uses: 3,
+          actionCost: 1
+        }
+      }
+    ],
+
+    conditional: [
+      {
+        id: "story_gundam_nt_sense",
+        label: "ニュータイプ感性",
+        cost: 20,
+        detail: "5ターンに1度、相手のスロット行動を予測し、成功した時回避+3を得る。",
+        data: {
+          kind: "create_skill",
+          effectId: "nt_sense_prediction",
+          cooldown: 5,
+          evadeGain: 3
+        }
+      }
+    ]
   },
 
   forms: {
